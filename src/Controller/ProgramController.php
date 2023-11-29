fa463f485d8048051d240c909361cb747c262176
[Ryad] <[ryadzaatour@gmail.com]>
2023-11-29 11:37:28 +0100

    fix quete-9

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,12 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProgramRepository;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
-    public function index(): Response
+    //#[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findAll();
+        
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
          ]);
