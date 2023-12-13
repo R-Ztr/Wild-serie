4c52db8f6fabf2c81f82db085434d4204f2ffbdf
[Ryad] <[ryadzaatour@gmail.com]>
2023-12-13 15:49:55 +0100

    quete-21

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -14,7 +14,8 @@ use App\Service\ProgramDuration;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
-//use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/program', name: 'program_')]
@@ -41,17 +42,28 @@ class ProgramController extends AbstractController
     }
 
     #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger) : Response
+    public function new(Request $request, MailerInterface $mailer, EntityManagerInterface $entityManager, SluggerInterface $slugger, ProgramRepository $programRepository) : Response
     {
         $program = new Program();
         $form = $this->createForm(ProgramType::class, $program);
         $form->handleRequest($request);
         
         if ($form->isSubmitted() && $form->isValid()) {
+
             $slug = $slugger->slug($program->getTitle());
             $program->setSlug($slug);
             $entityManager->persist($program);
             $entityManager->flush();
+
+           // $programRepository->save($program, true); 
+
+            $email = (new Email())
+            ->from($this->getParameter('mailer_from'))
+            ->to('your_email@example.com')
+            ->subject('Une nouvelle série vient d\'être publiée !')
+            ->html('<p>Une nouvelle série vient d\'être publiée sur Wild Séries !</p>');
+             $mailer->send($email);
+
     
             $this->addFlash('success', 'The new program has been created');
 
